50e347e6afa4bc94f511528f540e58e8b2ad5838
TechCentreUK <support@techcentreuk.co.uk>
2021-08-29 20:15:27 +0000

    Add Global Variables

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 # Global Variables
 
+# Our New Game Board
+board = ["*", "*", "*",
+         "*", "*", "*",
+         "*", "*", "*"]
+
+# Game in progress / finsihed
+ongoing_game = True
+
+# Game winner
+winner = None
+
+# Current Player (X starts first)
+current_player = "X"
+
 # Functions
 
 # Execute
