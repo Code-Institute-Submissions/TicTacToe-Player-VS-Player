9554868b67ffc0f97730be49240db7c0b8213600
TechCentreUK <support@techcentreuk.co.uk>
2021-08-29 20:35:23 +0000

    Add display_board function

--- run.py
+++ run.py
@@ -16,23 +16,34 @@ current_player = "X"
 
 # ------- Functions -------
 
-# Play a game of tic tac toe
+
+# 02 Display the game board to the screen
+def display_board():
+    print("\n")
+    print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
+    print("\n")
+
+
+# 01 Play a game of tic tac toe
 def play_game():
-  # Show the initial game board
-  display_board()
-  # Loop until the game stops (winner or tie)
-  while ongoing_game:
-    # Handle a turn
-    handle_turn(current_player)
-    # Check if the game is over
-    check_if_game_over()
-    # Swap to the other player
-    swap_player()
-  # Since the game is over, print the winner or tie
-  if winner == "X" or winner == "O":
-    print("Congratulations " + winner + ", you win.")
-  elif winner == None:
-    print("The game is a tie.")
+    # Show the initial game board
+    display_board()
+    # Loop until the game stops (winner or tie)
+    while ongoing_game:
+        # Handle a turn
+        handle_turn(current_player)
+        # Check if the game is over
+        check_if_game_over()
+        # Swap to the other player
+        swap_player()
+        # Since the game is over, print the winner or tie
+        if winner == "X" or winner == "O":
+            print("Congratulations " + winner + ", you win.")
+        elif winner == None:
+            print("The game is a tie.")
+
 
 # ------- Execute -------
 
