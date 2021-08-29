39078897ae828e7a0671ba5778664013b35d9446
TechCentreUK <support@techcentreuk.co.uk>
2021-08-29 20:27:32 +0000

    Add starting function and execute function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# Global Variables
+# ------- Global Variables -------
 
 # Our New Game Board
 board = ["*", "*", "*",
@@ -14,6 +14,27 @@ winner = None
 # Current Player (X starts first)
 current_player = "X"
 
-# Functions
+# ------- Functions -------
 
-# Execute
+# Play a game of tic tac toe
+def play_game():
+  # Show the initial game board
+  display_board()
+  # Loop until the game stops (winner or tie)
+  while ongoing_game:
+    # Handle a turn
+    handle_turn(current_player)
+    # Check if the game is over
+    check_if_game_over()
+    # Swap to the other player
+    swap_player()
+  # Since the game is over, print the winner or tie
+  if winner == "X" or winner == "O":
+    print("Congratulations " + winner + ", you win.")
+  elif winner == None:
+    print("The game is a tie.")
+
+# ------- Execute -------
+
+# Start a game of tic tac toe
+play_game()
