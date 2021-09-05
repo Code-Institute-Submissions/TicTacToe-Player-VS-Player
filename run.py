fe7a7917382c207f0f74ef00de97c15a8e782cfa
TechCentreUK <support@techcentreuk.co.uk>
2021-09-05 20:27:43 +0000

    Add Doc Strings to all functions

--- run.py
+++ run.py
@@ -30,6 +30,9 @@ class SpaceTakenError(Error):
 
 
 def y_n_prompt():
+    """ Prompts the user with a Y/N question
+    to play another game
+    """
     start = input("Would you like to play a game? Y/N: ").upper()
     while True:
         try:
@@ -41,10 +44,8 @@ def y_n_prompt():
             print("Invalid character, Please try again")
             y_n_prompt()
         else:
-            # If yes play game
             if start.upper() == "Y":
                 play_game()
-            # if no exit
             elif start.upper() == "N":
                 print("\n")
                 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -54,10 +55,12 @@ def y_n_prompt():
                 exit()
 
 
-# Play again when game ends
 def play_again():
     print("\n")
-    # Ask user for another game
+    """ Ask user for another game,
+    if yes resets board
+    and if no exits out
+    """
     play = input("Would you like to play another game? Y/N: ").upper()
     while True:
         try:
@@ -69,23 +72,16 @@ def play_again():
             print("Invalid character, Please try again")
             y_n_prompt()
         else:
-            # If yes reset the game
             if play.upper() == "Y":
-                # Reset board
                 global board
                 board = ["*", "*", "*", "*", "*", "*", "*", "*", "*"]
-                # Reset ongoing_game
                 global ongoing_game
                 ongoing_game = True
-                # Reset current player back to X
                 global current_player
                 current_player = "X"
-                # Reset winner to none
                 global winner
                 winner = None
-                # Run Game
                 play_game()
-                # Exit if N was typed
             elif play.upper() == "N":
                 print("\n")
                 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -95,106 +91,94 @@ def play_again():
                 exit()
 
 
-# Swap the current player from X to O, or O to X
 def swap_player():
-    # Global variable we need
+    """ Swap the current player from X to O, or O to X """
     global current_player
-    # If the current player was X, make it O
     if current_player == "X":
         current_player = "O"
-    # Or if the current player was O, make it X
     elif current_player == "O":
         current_player = "X"
 
 
-# Check if there is a tie
 def check_for_tie():
-    # Set global variable
+    """
+    Check for a winner and if any spaces left
+    if no winner and no spaces then returns a tie
+    """
     global ongoing_game
-    # If board is full stop game
     check_for_winner()
     if "*" not in board and winner is None:
         ongoing_game = False
-        # And print game is a tie
         print("Game is a Tie! \n")
         play_again()
         return True
-    # Else there is no tie
     else:
         return False
 
 
-# Check the diagonals for a win
 def check_diagonals():
-    # Set global variable
+    """
+    Check all diagonal rows for a winner
+    if no winner then returns None
+    """
     global ongoing_game
-    # Check if any of the columns have all the same value (and is not empty)
     diagonal_1 = board[0] == board[4] == board[8] != "*"
     diagonal_2 = board[2] == board[4] == board[6] != "*"
-    # If any diagonal row has a match, flag that there is a win
     if diagonal_1 or diagonal_2:
         ongoing_game = False
-    # Return the winner
     if diagonal_1:
         return board[0]
     elif diagonal_2:
         return board[2]
-    # Or return None if there was no winner
     else:
         return None
 
 
-# Check the columns for a win
 def check_columns():
-    # Set global variable
+    """
+    Check all columns for a winner
+    if no winner then returns None
+    """
     global ongoing_game
-    # Check if any of the columns have all the same value (and is not empty)
     column_1 = board[0] == board[3] == board[6] != "*"
     column_2 = board[1] == board[4] == board[7] != "*"
     column_3 = board[2] == board[5] == board[8] != "*"
-    # If any columns have a match, flag that there is a win
     if column_1 or column_2 or column_3:
         ongoing_game = False
-    # Return the winner
     if column_1:
         return board[0]
     elif column_2:
         return board[1]
     elif column_3:
         return board[2]
-    # Or return None if there was no winner
     else:
         return None
 
 
-# Check the rows for a win
 def check_rows():
-    # Set global variable
+    """
+    Check all rows for a winner
+    if no winner then returns None
+    """
     global ongoing_game
-    # Check if any of the rows have all the same value (and is not empty)
     row_1 = board[0] == board[1] == board[2] != "*"
     row_2 = board[3] == board[4] == board[5] != "*"
     row_3 = board[6] == board[7] == board[8] != "*"
-    # If any rows have a match, flag that there is a win
     if row_1 or row_2 or row_3:
         ongoing_game = False
-    # Return the winner
     if row_1:
         return board[0]
     elif row_2:
         return board[3]
     elif row_3:
         return board[6]
-    # Or return None if there was no winner
     else:
         return None
 
 
-# Check for winner
 def check_for_winner():
-    # Set global variable
+    """ Check all possible outcomes for a winner """
     global winner
-    # Check rows, columns and diagonals for a winning line
     row_winner = check_rows()
     column_winner = check_columns()
     diagonal_winner = check_diagonals()
@@ -208,16 +192,17 @@ def check_for_winner():
         winner = None
 
 
-# Check for game end
 def check_if_game_over():
-    # Check for winner
+    """ Check the game for a winner or tie """
     check_for_winner()
-    # Check for tie
     check_for_tie()
 
 
-# Handle Turn
 def handle_turn(player):
+    """
+    Loops through players turns and
+    handles exception errors appropriately
+    """
     print(player + "'s turn.")
     valid = False
     position = input("\nChoose a position from 1-9: ")
@@ -243,8 +228,8 @@ def handle_turn(player):
             position = input("\nChoose a position from 1-9: ")
 
 
-# Display the game board to the screen
 def display_board():
+    """ Displays the game board """
     print("\n")
     print("-------------------------------------")
     print("|  " + board[0] + " | " + board[1] +
@@ -257,19 +242,16 @@ def display_board():
     print("\n")
 
 
-# Play a game of tic tac toe
 def play_game():
-    # Show the initial game board
+    """Play a game of tic tac toe, game displays the board,
+    loops the turns whilst game is in session then checks the
+    game is over. If game is ongoing then swaps player until a winner.
+    """
     display_board()
-    # Loop until the game stops (winner or tie)
     while ongoing_game:
-        # Handle a turn
         handle_turn(current_player)
-        # Check if the game is over
         check_if_game_over()
-        # Swap to the other player
         swap_player()
-        # Since the game is over, print the winner or tie
         global board
         if winner == "X" or winner == "O":
             print("<-------- Congratulations " +
@@ -277,8 +259,8 @@ def play_game():
             play_again()
 
 
-# Prompt user to start game
 def start():
+    """ Prompt user for a game of tic tac toe """
     display_board()
     print("\n")
     y_n_prompt()
@@ -287,6 +269,5 @@ def start():
 # ------- Execute -------
 
 # Start a game of tic tac toe
-print("\n")
-print("Welcome to Clayton's TicTacToe Multiplayer Game")
+print("\nWelcome to Clayton's TicTacToe Multiplayer Game")
 start()
